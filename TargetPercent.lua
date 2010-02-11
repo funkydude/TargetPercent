7baec2a7fdd5bcdd0b96708b751a91501e451ae2
funkydude <funkydude@users.noreply.github.com>
2010-02-11 22:14:02 +0000

    Show one decimal place

--- TargetPercent.lua
+++ TargetPercent.lua
@@ -1,13 +1,13 @@
 
 do
 	local showFrame = CreateFrame("Frame", "TargetPercent", TargetFrameHealthBar)
-	showFrame:SetPoint("LEFT", TargetFrameHealthBar, "LEFT", -40, 0)
-	showFrame:SetWidth(40)
+	showFrame:SetPoint("LEFT", TargetFrameHealthBar, "LEFT", -46, 0)
+	showFrame:SetWidth(45)
 	showFrame:SetHeight(20)
 	showFrame.text = showFrame:CreateFontString("TargetPercentText", "OVERLAY")
 	showFrame.text:SetAllPoints(showFrame)
 	showFrame.text:SetFontObject(TextStatusBarText)
-	showFrame.text:SetJustifyH("CENTER")
+	showFrame.text:SetJustifyH("RIGHT")
 	showFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 	showFrame:RegisterEvent("UNIT_HEALTH")
 	showFrame:SetScript("OnEvent", function(frame, _, unit)
@@ -15,7 +15,7 @@ do
 		local hp = UnitHealth("target")
 		if hp > 0 then
 			hp = hp / UnitHealthMax("target") * 100
-			frame.text:SetFormattedText("%d%%", hp)
+			frame.text:SetFormattedText("%.1f%%", hp)
 		else
 			frame.text:SetText("0%")
 		end

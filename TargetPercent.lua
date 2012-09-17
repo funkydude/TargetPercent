d221709eb4493d3ff29e38a5e9d5fc473aae0f7c
funkydude <funkydude@users.noreply.github.com>
2012-09-17 02:08:20 +0000

    re-write to use RegisterUnitEvent

--- TargetPercent.lua
+++ TargetPercent.lua
@@ -4,26 +4,25 @@ local name, addon = ...
 local startDrag = function(frame) if IsAltKeyDown() then frame:StartMoving() end end
 local stopDrag = function(frame) frame:StopMovingOrSizing() end
 
+local UnitHealth, UnitHealthMax = UnitHealth, UnitHealthMax
+
 local healthUpdate = function(frame, _, unit)
 	unit = unit or frame.unit
-	if addon[unit] then
-		local hp = UnitHealth(unit)
-		if hp > 0 then
-			hp = hp / UnitHealthMax(unit) * 100
-			addon[unit]:SetFormattedText("%.1f%%", hp)
-		else
-			addon[unit]:SetText("0%")
-		end
+	local hp = UnitHealth(unit)
+	if hp > 0 then
+		hp = hp / UnitHealthMax(unit) * 100
+		addon[unit]:SetFormattedText("%.1f%%", hp)
+	else
+		addon[unit]:SetText("0%")
 	end
 end
 
 addon.target = CreateFrame("Frame", name, TargetFrameHealthBar)
-addon.target:RegisterEvent("UNIT_HEALTH_FREQUENT")
-
+addon.target:RegisterEvent("PLAYER_TARGET_CHANGED")
+addon.target:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "target")
 addon.target:SetPoint("LEFT", TargetFrameHealthBar, "LEFT", -51, 0)
 addon.target:SetWidth(50)
 addon.target:SetHeight(20)
-addon.target:RegisterEvent("PLAYER_TARGET_CHANGED")
 addon.target:EnableMouse(true)
 addon.target:RegisterForDrag("LeftButton")
 addon.target:SetClampedToScreen(true)
@@ -31,6 +30,7 @@ addon.target:SetMovable(true)
 addon.target:SetScript("OnDragStart", startDrag)
 addon.target:SetScript("OnDragStop", stopDrag)
 addon.target:SetScript("OnEvent", healthUpdate)
+addon.target:SetScript("OnShow", healthUpdate)
 addon.target.unit = "target"
 addon.target = addon.target:CreateFontString("TargetPercentText", "OVERLAY")
 addon.target:SetAllPoints(name)
@@ -38,10 +38,11 @@ addon.target:SetFontObject(TextStatusBarText)
 addon.target:SetJustifyH("RIGHT")
 
 addon.focus = CreateFrame("Frame", "FocusPercent", FocusFrameHealthBar)
+addon.focus:RegisterEvent("PLAYER_FOCUS_CHANGED")
+addon.focus:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "focus")
 addon.focus:SetPoint("LEFT", FocusFrameHealthBar, "LEFT", -51, 0)
 addon.focus:SetWidth(50)
 addon.focus:SetHeight(20)
-addon.focus:RegisterEvent("PLAYER_FOCUS_CHANGED")
 addon.focus:EnableMouse(true)
 addon.focus:RegisterForDrag("LeftButton")
 addon.focus:SetClampedToScreen(true)
@@ -49,6 +50,7 @@ addon.focus:SetMovable(true)
 addon.focus:SetScript("OnDragStart", startDrag)
 addon.focus:SetScript("OnDragStop", stopDrag)
 addon.focus:SetScript("OnEvent", healthUpdate)
+addon.focus:SetScript("OnShow", healthUpdate)
 addon.focus.unit = "focus"
 addon.focus = addon.focus:CreateFontString("FocusPercentText", "OVERLAY")
 addon.focus:SetAllPoints("FocusPercent")
@@ -56,6 +58,7 @@ addon.focus:SetFontObject(TextStatusBarText)
 addon.focus:SetJustifyH("RIGHT")
 
 addon.boss1 = CreateFrame("Frame", "Boss1Percent", Boss1TargetFrameHealthBar)
+addon.boss1:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "boss1")
 addon.boss1:SetPoint("LEFT", Boss1TargetFrameHealthBar, "LEFT", -51, 0)
 addon.boss1:SetWidth(50)
 addon.boss1:SetHeight(20)
@@ -65,12 +68,15 @@ addon.boss1:SetClampedToScreen(true)
 addon.boss1:SetMovable(true)
 addon.boss1:SetScript("OnDragStart", startDrag)
 addon.boss1:SetScript("OnDragStop", stopDrag)
+addon.boss1:SetScript("OnEvent", healthUpdate)
+addon.boss1:SetScript("OnShow", healthUpdate)
 addon.boss1 = addon.boss1:CreateFontString("Boss1PercentText", "OVERLAY")
 addon.boss1:SetAllPoints("Boss1Percent")
 addon.boss1:SetFontObject(TextStatusBarText)
 addon.boss1:SetJustifyH("RIGHT")
 
 addon.boss2 = CreateFrame("Frame", "Boss2Percent", Boss2TargetFrameHealthBar)
+addon.boss2:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "boss2")
 addon.boss2:SetPoint("LEFT", Boss2TargetFrameHealthBar, "LEFT", -51, 0)
 addon.boss2:SetWidth(50)
 addon.boss2:SetHeight(20)
@@ -80,12 +86,15 @@ addon.boss2:SetClampedToScreen(true)
 addon.boss2:SetMovable(true)
 addon.boss2:SetScript("OnDragStart", startDrag)
 addon.boss2:SetScript("OnDragStop", stopDrag)
+addon.boss2:SetScript("OnEvent", healthUpdate)
+addon.boss2:SetScript("OnShow", healthUpdate)
 addon.boss2 = addon.boss2:CreateFontString("Boss2PercentText", "OVERLAY")
 addon.boss2:SetAllPoints("Boss2Percent")
 addon.boss2:SetFontObject(TextStatusBarText)
 addon.boss2:SetJustifyH("RIGHT")
 
 addon.boss3 = CreateFrame("Frame", "Boss3Percent", Boss3TargetFrameHealthBar)
+addon.boss3:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "boss3")
 addon.boss3:SetPoint("LEFT", Boss3TargetFrameHealthBar, "LEFT", -51, 0)
 addon.boss3:SetWidth(50)
 addon.boss3:SetHeight(20)
@@ -95,12 +104,15 @@ addon.boss3:SetClampedToScreen(true)
 addon.boss3:SetMovable(true)
 addon.boss3:SetScript("OnDragStart", startDrag)
 addon.boss3:SetScript("OnDragStop", stopDrag)
+addon.boss3:SetScript("OnEvent", healthUpdate)
+addon.boss3:SetScript("OnShow", healthUpdate)
 addon.boss3 = addon.boss3:CreateFontString("Boss3PercentText", "OVERLAY")
 addon.boss3:SetAllPoints("Boss3Percent")
 addon.boss3:SetFontObject(TextStatusBarText)
 addon.boss3:SetJustifyH("RIGHT")
 
 addon.boss4 = CreateFrame("Frame", "Boss4Percent", Boss4TargetFrameHealthBar)
+addon.boss4:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "boss4")
 addon.boss4:SetPoint("LEFT", Boss4TargetFrameHealthBar, "LEFT", -51, 0)
 addon.boss4:SetWidth(50)
 addon.boss4:SetHeight(20)
@@ -110,6 +122,8 @@ addon.boss4:SetClampedToScreen(true)
 addon.boss4:SetMovable(true)
 addon.boss4:SetScript("OnDragStart", startDrag)
 addon.boss4:SetScript("OnDragStop", stopDrag)
+addon.boss4:SetScript("OnEvent", healthUpdate)
+addon.boss4:SetScript("OnShow", healthUpdate)
 addon.boss4 = addon.boss4:CreateFontString("Boss4PercentText", "OVERLAY")
 addon.boss4:SetAllPoints("Boss4Percent")
 addon.boss4:SetFontObject(TextStatusBarText)
